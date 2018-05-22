e3585773668aa69f030bd14c547ae7db2496123d
Daniel Clasen <mail@danielclasen.de>
2018-05-22 14:37:54 +0200

    fix wallet arg regex match

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,25 +1,32 @@
 #!/usr/bin/env bash
 
-wallet_regex=".*-w\ (\S+).*|.*--container-file=(\S+).*"
+wallet_regex_short=".*-w\ (\S+).*"
+wallet_regex_long=".*--container-file[=,\ ](\S+).*"
 
-if [[ "$@" =~ $wallet_regex ]]; then
+WALLET=""
+
+if [[ "$@" =~ $wallet_regex_short ]]; then
+
     WALLET=${BASH_REMATCH[1]}
 
-    if [ ! -f "$WALLET" ]; then
-        echo "Wallet $WALLET does not exist. Going to create a new one."
-        /iridium/iridium_walletd "$@" -g
-        chmod -R 777 "$WALLET"
-    else
-        echo "Wallet $WALLET already present."
-    fi
+elif [[ "$@" =~ $wallet_regex_long ]]; then
 
-    echo "Going to launch walletd now..."
-
-    /iridium/iridium_walletd "$@"
-
-
+    WALLET=${BASH_REMATCH[1]}
 
 else
     echo "Please specify a wallet file!"
     exit 1;
 fi
+
+
+if [ ! -f "$WALLET" ]; then
+    echo "Wallet $WALLET does not exist. Going to create a new one."
+    /iridium/iridium_walletd "$@" -g
+    chmod -R 777 "$WALLET"
+else
+    echo "Wallet $WALLET already present."
+fi
+
+echo "Going to launch walletd now..."
+
+/iridium/iridium_walletd "$@"
